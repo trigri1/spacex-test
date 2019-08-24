ccfe8f24e07854e649d91e11c0eaf0093e9d5f6e
gohar-ali <gohar.ali@starzplayarabia.com>
2019-08-24 17:32:35 +0500

    initial commit and dependencies added

---
+++ app/src/main/java/com/test/spacex/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.test.spacex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.test.spacex.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
